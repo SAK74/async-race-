4dd8ce5f020846957258c81047eb4261a06a17e6
SAK74 <sergijks@gmail.com>
2024-04-18 20:17:52 +0200

    init: initialize project with eslint, prettier & tailwindcss

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+// @ts-check
+const { defineConfig } = require('eslint-define-config');
+
+module.exports = defineConfig({
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ['airbnb', 'airbnb-typescript', 'airbnb/hooks', 'prettier'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+});
---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Config}
+ */
+const config = {
+  endOfLine: "auto",
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 100,
+  arrowParens: "always",
+};
+
+module.exports = config;
